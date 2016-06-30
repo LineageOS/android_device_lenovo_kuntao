8506a92f1739756202e525cf658083f2a2e5b35b
Hamster Tian <haotia@gmail.com>
2016-06-30 13:22:22 +0800

    kuntao: add NFA_PROPRIETARY_CFG for proper Mifare Classic support
    * NFC stack in Android M has native support for Mifare Classic,
      but default protocol number is wrong. This property will correct
      the number for PN547C2.
    * Mostly copied from sample configuration, with
      NCI_DISCOVERY_TYPE_POLL_KOVIO changed to 0x77. Any other value will
      cause errorneous commands sent to NFCC, and NFC stack will fall into
      infinite loop.
    * PSE and Android Beam is working.
    
    Change-Id: I270457ac77145ab26fe2528e0b1ebc1b50b91d29

--- device.mk
+++ device.mk
@@ -198,6 +198,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
 # Power
