ff0fe610cc705f20463bf41019dad14a8563fd8b
Naveen Kumar <kumarn@codeaurora.org>
2016-08-21 23:31:32 +0530

    kuntao: Add fs_config support for custom binary
    Build custom target binary to override the default
    permission.
    
    Change-Id: Ia3d48bd21f7139c5b9e04261ab8f05b8ca792249

--- device.mk
+++ device.mk
@@ -80,6 +80,10 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
+# For android_filesystem_config.h
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # GPS
 PRODUCT_PACKAGES += \
     libgnsspps \
