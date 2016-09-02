673df785df0a7adda5b344f508549ab42f11d2ba
Srinu Gorle <sgorle@codeaurora.org>
2016-09-02 17:48:46 +0530

    kuntao: disable PQ feature for video encoding by default
    Set the properties to false to disable PQ feature for video encoding.
    
    Change-Id: Ib5ef3558283962e99ff192a9f2f4836ea21d76d6
    CRs-Fixed: 1062824

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -98,7 +98,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.vidc.enc.disable_bframes=1 \
     vendor.vidc.enc.disable_pframes=1 \
     vendor.vidc.enc.narrow.searchrange=1 \
-    vendor.video.disable.ubwc=1
+    vendor.video.disable.ubwc=1 \
+    vidc.enc.disable.pq=true
 
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
