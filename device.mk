c44b334fe3a8cbbe5c74407cf6645e917cf1443a
Roshan Pius <rpius@google.com>
2016-12-08 18:11:09 -0800

    kuntao: Add wifi HAL service to device.mk
    Bug: 33460969
    Test: Compiles
    Change-Id: I473f00d5dcabbf84d2da7397aef69b1b688d28a9

--- device.mk
+++ device.mk
@@ -247,6 +247,9 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
