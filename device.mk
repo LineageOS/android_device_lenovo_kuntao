6da8b060520e5cb20df679f7e09ca3b2ec582ec4
Myles Watson <mylesgw@google.com>
2017-03-31 11:25:04 -0700

    kuntao: Add libbt-vendor in device-common.mk
    Bug: 36810308
    Test: build
    Change-Id: Iae4a6340a184dde818603b546470c8cf883f2f7b

--- device.mk
+++ device.mk
@@ -59,6 +59,9 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    libbt-vendor
+
+PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service
 
