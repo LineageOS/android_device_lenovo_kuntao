9ac22887fcf41da6f430b1edec52846efc13579b
Muhammed Siju <msiju@codeaurora.org>
2017-04-06 16:14:08 -0700

    kuntao: IMS: Moving ims daemons to vendor
    Change path of ims dameons to vendor in init scripts.
    
    Change-Id: Ia61a7f7442f5b99fd57378b68633fb3a22581ccf
    CRs-Fixed: 2031636

--- config.fs
+++ config.fs
@@ -19,13 +19,13 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/bin/imsdatadaemon]
+[vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/bin/ims_rtp_daemon]
+[vendor/bin/ims_rtp_daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
