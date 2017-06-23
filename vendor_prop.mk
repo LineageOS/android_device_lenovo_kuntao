dc050d9838f3f24c8f8a11a6ea913c85f3869d01
Bharat Pawar <bpawar@codeaurora.org>
2017-06-23 19:09:27 +0530

    kuntao: Set debug.sf.enable_hwc_vds property to true
    Set above property to enable wfd use hwc.
    
    Change-Id: I0155374536153c6b05333b1acb10aef86d321c8c

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -63,6 +63,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.gralloc.enable_fb_ubwc=1 \
+    debug.sf.enable_hwc_vds=1 \
     ro.opengles.version=196610 \
     ro.qualcomm.cabl=0 \
     ro.sf.lcd_density=480 \
