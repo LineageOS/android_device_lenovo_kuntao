6362e566f17c92291b6f1c12027554a584dfbcdf
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-07-08 23:42:51 +0800

    kuntao: camera: Fixes to OSS camera hal to build in LineageOS tree
    * Fix some include paths
    * Fix CleanSpec camera HAL name
    
    Change-Id: I9ce7744a23c059cef5221a506173f88099a30bb7

--- camera/QCamera2/HAL/test/Android.mk
+++ camera/QCamera2/HAL/test/Android.mk
@@ -37,7 +37,7 @@ LOCAL_C_INCLUDES += \
     external/skia/include/core \
     external/skia/include/images \
     $(TARGET_OUT_HEADERS)/qcom/display \
-    hardware/qcom/camera/QCamera2/stack/common \
+    $(LOCAL_PATH)/../stack/common \
     frameworks/av/include/media/stagefright \
     frameworks/native/include/media/openmax \
     $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
