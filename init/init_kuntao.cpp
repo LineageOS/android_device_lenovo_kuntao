19d99fbd653f3695b4bc493dc9b7bdf3ec8f0271
Luca Stefani <luca020400@lineageos.org>
2017-08-18 11:39:28 +0200

    kuntao: init: Update for O
    Change-Id: I635dc8d27f92f9a2fa83da62c2f32e48bd2c7680

--- init/init_kuntao.cpp
+++ init/init_kuntao.cpp
@@ -29,13 +29,15 @@
 
 #include <cstdio>
 
+#include <android-base/file.h>
+#include <android-base/properties.h>
 #include <android-base/strings.h>
 
-#include "vendor_init.h"
 #include "property_service.h"
-#include "log.h"
-#include "util.h"
+#include "vendor_init.h"
 
+using android::base::GetProperty;
+using android::base::ReadFileToString;
 using android::base::Trim;
 
 static void init_alarm_boot_properties()
@@ -44,10 +46,10 @@ static void init_alarm_boot_properties()
     char const *power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
     std::string boot_reason;
     std::string power_off_alarm;
-    std::string tmp = property_get("ro.boot.alarmboot");
+    std::string reboot_reason = GetProperty("ro.boot.alarmboot", "");
 
-    if (read_file(boot_reason_file, &boot_reason)
-            && read_file(power_off_alarm_file, &power_off_alarm)) {
+    if (ReadFileToString(boot_reason_file, &boot_reason)
+            && ReadFileToString(power_off_alarm_file, &power_off_alarm)) {
         /*
          * Setup ro.alarm_boot value to true when it is RTC triggered boot up
          * For existing PMIC chips, the following mapping applies
@@ -58,12 +60,12 @@ static void init_alarm_boot_properties()
          * 2 -> sudden momentary power loss (SMPL)
          * 3 -> real time clock (RTC)
          * 4 -> DC charger inserted
-         * 5 -> USB charger insertd
+         * 5 -> USB charger inserted
          * 6 -> PON1 pin toggled (for secondary PMICs)
          * 7 -> CBLPWR_N pin toggled (for external power supply)
          * 8 -> KPDPWR_N pin toggled (power key pressed)
          */
-        if ((Trim(boot_reason) == "3" || tmp == "true")
+        if ((Trim(boot_reason) == "3" || reboot_reason == "true")
                 && Trim(power_off_alarm) == "1")
             property_set("ro.alarm_boot", "true");
         else
