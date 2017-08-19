85372f106228ddbc5dd28561011ca72ff30c1f92
Luca Stefani <luca020400@lineageos.org>
2017-08-19 13:17:46 +0200

    kuntao: Inherit from Lineage vendor
    Change-Id: I6d60ee5643181781ba8b386f1f16cfb59610f35c

--- lineage.mk
+++ lineage.mk
@@ -25,7 +25,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lenovo/kuntao/device.mk)
