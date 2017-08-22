d0f300a47273c18503e32f817e0157bb5a6c0fd3
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:03:01 +0200

    kuntao: Build Display HIDL HAL interfaces
     * Enable binderized Gralloc HAL along with it
    
    Change-Id: I61c34a531e3131209a08b5b4670e1f1f8fbb435d

--- device.mk
+++ device.mk
@@ -80,6 +80,15 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     memtrack.msm8953
 
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
+
 # Display calibration
 PRODUCT_PACKAGES += \
     libjni_livedisplay
