3cf235dfbe078ef03cbca6798992fe304be09d5c
FireLord1 <firelord.xda@gmail.com>
2017-08-26 23:54:28 +0530

    kuntao: Import liblights from display-caf
    Change-Id: I32db6901dfab86e3a1ff3f7db2161635b337be5d
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,6 +111,9 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := lineageos_kuntao_defconfig
 TARGET_KERNEL_SOURCE := kernel/lenovo/msm8953
 
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
