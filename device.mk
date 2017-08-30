44a9c8cbd1bb975ed5df5448be7bae65506dd50c
nicknitewolf <nicknitewolf@lineageos.org>
2017-08-30 17:00:16 +0800

    kuntao: Change WCNSS_qcom_cfg.ini file path
    Change-Id: I48381daba9c839379302e638cc8a3d6388563f26

--- device.mk
+++ device.mk
@@ -332,7 +332,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Call the proprietary setup
 $(call inherit-product, vendor/lenovo/kuntao/kuntao-vendor.mk)
