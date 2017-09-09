UBERCAF/vendor_qcom_opensource_location

Handle confidence value for circular uncertainity
The circular uncertainity has a 63% confidence and so we need to
use a scaling factor of 1.07 if we are using the circular value.

Change-Id: I88048656b9b983e2a510d600ce95632c92e467e7
CRs-Fixed: 1107075

loc_api/loc_api_v02/LocApiV02.cpp
@@ -2192,8 +2192,11 @@ void LocApiV02 :: reportPosition (
             // then scale the accuracy value to 68% confidence.
             if (location_report_ptr->horConfidence_valid)
             {
+                bool is_CircUnc = (location_report_ptr->horUncCircular_valid) ?
+                                                                        true : false;
                 scaleAccuracyTo68PercentConfidence(location_report_ptr->horConfidence,
-                                                   location.gpsLocation);
+                                                   location.gpsLocation,
+                                                   is_CircUnc);
             }
 
             // Technology Mask
@@ -3256,16 +3259,21 @@ void LocApiV02 :: reportNiRequest(
    68%.confidence.*/
 void LocApiV02 :: scaleAccuracyTo68PercentConfidence(
                                                 const uint8_t confidenceValue,
-                                                LocGpsLocation &gpsLocation)
+                                                LocGpsLocation &gpsLocation,
+                                                const bool isCircularUnc)
 {
   if (confidenceValue < 68)
   {
+    // Circular uncertainty is at 63%.confidence. Scale factor should be
+    // 1.072(from 63% -> 68%)
+    uint8_t realConfidence = (isCircularUnc) ? 63:confidenceValue;
     // get scaling value based on 2D% confidence scaling table
     for (uint8_t iter = 0; iter < CONF_SCALER_ARRAY_MAX; iter++)
     {
-      if (confidenceValue <= confScalers[iter].confidence)
+      if (realConfidence <= confScalers[iter].confidence)
       {
-        LOC_LOGD("Scaler value:%f",confScalers[iter].scaler_to_68);
+        LOC_LOGD("Confidence: %d, Scaler value:%f",
+                realConfidence,confScalers[iter].scaler_to_68);
         gpsLocation.accuracy *= confScalers[iter].scaler_to_68;
         break;
       }
@@ -4116,7 +4124,7 @@ getBestAvailableZppFix(LocGpsLocation &zppLoc, LocPosTechMask &tech_mask)
             if (zpp_ind.horCircularConfidence_valid)
             {
                 scaleAccuracyTo68PercentConfidence(zpp_ind.horCircularConfidence,
-                                                   zppLoc);
+                                                   zppLoc, true);
             }
 
             if (zpp_ind.altitudeWrtEllipsoid_valid) {
@@ -4534,7 +4542,8 @@ handleWwanZppFixIndication(const qmiLocGetAvailWwanPositionIndMsgT_v02& zpp_ind)
     // is less than 68%, then scale the accuracy value to 68% confidence.
     if (zpp_ind.horCircularConfidence_valid)
     {
-        scaleAccuracyTo68PercentConfidence(zpp_ind.horCircularConfidence,zppLoc);
+        scaleAccuracyTo68PercentConfidence(zpp_ind.horCircularConfidence,
+                                           zppLoc, true);
     }
 
     if (zpp_ind.altitudeWrtEllipsoid_valid) {

loc_api/loc_api_v02/LocApiV02.h
@@ -94,7 +94,8 @@ private:
   /* If Confidence value is less than 68%, then scale the accuracy value to 68%
      confidence.*/
   void scaleAccuracyTo68PercentConfidence(const uint8_t confidenceValue,
-                                          LocGpsLocation &gpsLocation);
+                                          LocGpsLocation &gpsLocation,
+                                          const bool isCircularUnc);
 
   /* convert position report to loc eng format and send the converted
      position to loc eng */

LineageOS/android_device_lenovo_kuntao

loc_api/loc_api_v02/LocApiV02.cpp
@@ -2187,8 +2187,11 @@ void LocApiV02 :: reportPosition (
             // then scale the accuracy value to 68% confidence.
             if (location_report_ptr->horConfidence_valid)
             {
+                bool is_CircUnc = (location_report_ptr->horUncCircular_valid) ?
+                                                                        true : false;
                 scaleAccuracyTo68PercentConfidence(location_report_ptr->horConfidence,
-                                                   location.gpsLocation);
+                                                   location.gpsLocation,
+                                                   is_CircUnc);
             }
 
             // Technology Mask
@@ -3251,16 +3254,21 @@ void LocApiV02 :: reportNiRequest(
    68%.confidence.*/
 void LocApiV02 :: scaleAccuracyTo68PercentConfidence(
                                                 const uint8_t confidenceValue,
-                                                GpsLocation &gpsLocation)
+                                                GpsLocation &gpsLocation,
+                                                const bool isCircularUnc)
 {
   if (confidenceValue < 68)
   {
+    // Circular uncertainty is at 63%.confidence. Scale factor should be
+    // 1.072(from 63% -> 68%)
+    uint8_t realConfidence = (isCircularUnc) ? 63:confidenceValue;
     // get scaling value based on 2D% confidence scaling table
     for (uint8_t iter = 0; iter < CONF_SCALER_ARRAY_MAX; iter++)
     {
-      if (confidenceValue <= confScalers[iter].confidence)
+      if (realConfidence <= confScalers[iter].confidence)
       {
-        LOC_LOGD("Scaler value:%f",confScalers[iter].scaler_to_68);
+        LOC_LOGD("Confidence: %d, Scaler value:%f",
+                realConfidence,confScalers[iter].scaler_to_68);
         gpsLocation.accuracy *= confScalers[iter].scaler_to_68;
         break;
       }
@@ -4043,7 +4051,8 @@ getWwanZppFix(GpsLocation &zppLoc)
     // is less than 68%, then scale the accuracy value to 68% confidence.
     if (zpp_ind.horCircularConfidence_valid)
     {
-        scaleAccuracyTo68PercentConfidence(zpp_ind.horCircularConfidence,zppLoc);
+        scaleAccuracyTo68PercentConfidence(zpp_ind.horCircularConfidence,
+                                           zppLoc, true);
     }
 
     if (zpp_ind.altitudeWrtEllipsoid_valid) {
@@ -4130,7 +4139,7 @@ getBestAvailableZppFix(GpsLocation &zppLoc, LocPosTechMask &tech_mask)
             if (zpp_ind.horCircularConfidence_valid)
             {
                 scaleAccuracyTo68PercentConfidence(zpp_ind.horCircularConfidence,
-                                                   zppLoc);
+                                                   zppLoc, true);
             }
 
             if (zpp_ind.altitudeWrtEllipsoid_valid) {

loc_api/loc_api_v02/LocApiV02.h
@@ -93,7 +93,8 @@ private:
   /* If Confidence value is less than 68%, then scale the accuracy value to 68%
      confidence.*/
   void scaleAccuracyTo68PercentConfidence(const uint8_t confidenceValue,
-                                          GpsLocation &gpsLocation);
+                                          GpsLocation &gpsLocation,
+                                          const bool isCircularUnc);
 
   /* convert position report to loc eng format and send the converted
      position to loc eng */