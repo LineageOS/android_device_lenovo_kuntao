81ae41ee692242d2909e67c797579a57a8aa6ae3
Michael Bestas <mkbestas@lineageos.org>
2017-09-10 17:23:28 +0300

    kuntao: Cleanup gpio-keys and add qpnp_pon keylayout
    Change-Id: I2f0306895af51cda27f310628d1d510e06504037

--- device.mk
+++ device.mk
@@ -119,6 +119,7 @@ PRODUCT_COPY_FILES += \
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl \
     $(LOCAL_PATH)/keylayout/synaptics_dsx_i2c.kl:system/usr/keylayout/synaptics_dsx_i2c.kl
 
 # Media
