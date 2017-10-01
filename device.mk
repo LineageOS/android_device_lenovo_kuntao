e7dd768a16d0b290e4ee0164f49e8fc1fd865adf
Michael Bestas <mkbestas@lineageos.org>
2017-10-01 20:28:34 +0300

    kuntao: Initial overlays
    Change-Id: I3cd61c4945d0ce52d0ed7ccf62ead1fdad8298cf

--- device.mk
+++ device.mk
@@ -15,7 +15,9 @@
 #
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
