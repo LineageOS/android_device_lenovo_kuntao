2193a654e96120008df64458f1a0d93065bc39a1
Michael Bestas <mkbestas@lineageos.org>
2017-10-01 19:33:50 +0300

    kuntao: Build NFC packages
    * Using prebuilt HAL for now
    
    Change-Id: I27b1acf62f206106d619f0779148cd33ec055017

--- device.mk
+++ device.mk
@@ -123,6 +123,14 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
 
+# NFC
+PRODUCT_PACKAGES += \
+    libnfc \
+    libnfc_jni \
+    com.android.nfc_extras \
+    NfcNci \
+    Tag
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
