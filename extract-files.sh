cef804dff7a5e6bde20114b56a9cb9411319f7ce
Michael Bestas <mkbestas@lineageos.org>
2017-10-22 20:13:14 +0300

    kuntao: Initial blob list
    Change-Id: I26f0f1d36ee3f34136fdcca576814b144f4e6934

--- extract-files.sh
+++ extract-files.sh
@@ -59,6 +59,7 @@ fi
 # Initialize the helper
 setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false $clean_vendor
 
+extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 extract "$MY_DIR"/proprietary-files-twrp.txt "$SRC" "$SECTION"
 
 "$MY_DIR"/setup-makefiles.sh
