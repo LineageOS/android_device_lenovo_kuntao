e3331ee87e9a26cdc817568b0df23b6769b20349
Paul Keith <javelinanddart@gmail.com>
2017-12-03 21:24:15 +0100

    kuntao: Add KeyHandler to handle side switch
    Change-Id: I3550aabe51be9a07cf17fc9da22991cf36347311

---
+++ keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.settings.device;
+
+import android.content.Context;
+import android.view.KeyEvent;
+
+import com.android.internal.os.DeviceKeyHandler;
+
+public class KeyHandler implements DeviceKeyHandler {
+
+    private static final int KEY_KUNTAO_SWITCH = 249;
+
+    private static int lastAction = -1;
+
+    public KeyHandler(Context context) {
+        mContext = context;
+    }
+
+    public KeyEvent handleKeyEvent(KeyEvent event) {
+        int scanCode = event.getScanCode();
+        int action = event.getAction();
+
+        if (scanCode == KEY_KUNTAO_SWITCH && action != lastAction) {
+             if (action == KeyEvent.ACTION_UP) {
+                 // Switch turned on
+             } else if (action == KeyEvent.ACTION_DOWN) {
+                 // Switch turned off
+             }
+
+            lastAction = action;
+        }
+    }
+}
