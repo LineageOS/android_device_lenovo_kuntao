a2a3345ce5812be00e92369b8376dac48a7e2de8
Billchen1977 <billchen1977@gmail.com>
2017-12-05 22:05:11 +0530

    kuntao: Add btsco playback
     * disable sco enhanced commands
    
    Change-Id: I8c24fda21ee4fc49f1edfb64746172638353a23e

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,11 +17,12 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME        "Lenovo P2"
+#define BTM_DEF_LOCAL_NAME             "Lenovo P2"
 
-#define BLE_VND_INCLUDED          TRUE
-#define BLUETOOTH_QTI_SW          TRUE
-#define BT_CLEAN_TURN_ON_DISABLED TRUE
-#define MAX_ACL_CONNECTIONS       16
+#define BLE_VND_INCLUDED               TRUE
+#define BLUETOOTH_QTI_SW               TRUE
+#define BT_CLEAN_TURN_ON_DISABLED      TRUE
+#define BTM_SCO_ENHANCED_SYNC_DISABLED TRUE
+#define MAX_ACL_CONNECTIONS            16
 
 #endif
