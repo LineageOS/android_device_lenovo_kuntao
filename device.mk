d917740d612a574af3dd34b3e66f231184e9f53d
Bruno Martins <bgcngm@gmail.com>
2018-02-06 23:13:59 +0000

    kuntao: Build health HAL
    Change-Id: I9609aa4dd030f2b21eafc9e4975a0fde2f9bea81

--- device.mk
+++ device.mk
@@ -144,6 +144,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
 
+# Healthd
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service
+
 # HWUI
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
