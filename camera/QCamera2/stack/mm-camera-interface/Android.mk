bd9d8898830d2250f9e9a755205083feb0cc590e
Abhishek Jain <abhij@codeaurora.org>
2018-04-03 19:18:26 +0530

    kuntao: qcom-camera: Completed FR49059 and FR49058
    FR49059: Accessing AOSP/Google projects Headers
    
    Description: Vendor modules cannot refer AOSP header
    paths with absolute or Global Paths instead add
    corresponding header library
    
    FR49058 :  VNDK compliance
    
    Description : VNDK is set of AOSP libraries which
    are exposed to vendor (partition) for development.
    
    Example :
    
    error: vendor lib "/vendor/lib/libeffects.so"
     depends on non-eligible lib
     "/system/lib/libeffectsconfig.so".
    Here , QCOM vendor-partition Library
    using lib outside VNDK from system.
    
    CRs-Fixed: 2217547
    
    Change-Id: I51a88145fccba592080d6287ae7ff14d12c65296

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -34,8 +34,6 @@ LOCAL_COPY_HEADERS += ../common/cam_types.h
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/inc \
     $(LOCAL_PATH)/../common \
-    hardware/libhardware/include/hardware \
-    hardware/qcom/media/mm-core/inc \
     system/media/camera/include \
 
 LOCAL_CFLAGS += -DCAMERA_ION_HEAP_ID=ION_IOMMU_HEAP_ID
@@ -50,8 +48,9 @@ LOCAL_CFLAGS += -Wall -Wextra -Werror
 LOCAL_SRC_FILES := $(MM_CAM_FILES)
 
 LOCAL_MODULE           := libmmcamera_interface
-LOCAL_SHARED_LIBRARIES := libdl libcutils liblog libutils
+LOCAL_SHARED_LIBRARIES := libdl libcutils liblog
 LOCAL_HEADER_LIBRARIES := libhardware_headers
+LOCAL_HEADER_LIBRARIES += media_plugin_headers
 LOCAL_HEADER_LIBRARIES += generated_kernel_headers
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
