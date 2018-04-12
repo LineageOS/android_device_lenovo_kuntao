b0ad6197b6a8b3031c3a4aa5647764037392ee80
Bruno Martins <bgcngm@gmail.com>
2018-04-12 11:13:39 +0100

    kuntao: Switch to HIDL ANT+ HAL
    Change-Id: I0e0252b6e2c7e3e7da1e8508f010b7e6c2bfd128

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,7 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 TARGET_OTA_ASSERT_DEVICE := kuntao,kuntao_row,P2a42,p2,p2a42
 
 # ANT+
-BOARD_ANT_WIRELESS_DEVICE := "qualcomm-uart"
+BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
 # Audio
 AUDIO_FEATURE_ENABLED_AAC_ADTS_OFFLOAD := true
