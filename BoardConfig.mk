f15755d492faf57ef3eea6d04fe8b6d6d1694186
Vinay Verma <vvinay@codeaurora.org>
2018-04-16 07:56:07 +0530

    kuntao: Add MATRIX file for vendor HAL's
    Change-Id: I6c7eed54f066e61ad8039eef5ee6081e6de43560

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,6 +105,7 @@ TARGET_USES_HWC2 := true
 TARGET_USES_ION := true
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
