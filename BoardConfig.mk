8c67b75fb50b4412605bd27d16cb1b54de7b404a
Vinay Verma <vvinay@codeaurora.org>
2018-04-30 22:23:13 +0530

    kuntao: Mount dsp and firmware in vendor
    Change fstab to mount dsp and firmware to /vendor/dsp and
    /vendor/firmware_mnt.
    
    Add classpath for firmware loading /vendor/firmware_mnt/image in
    kernel command line.
    
    Change-Id: I2dd2e7ea6334fb979ad8ff8c755648748de93ab4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,6 +115,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_kuntao
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci
+BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_OFFSET = 0x00008000
 BOARD_KERNEL_PAGESIZE := 2048
