f5fa979ee54c3c94f7afd8e3b386d5da78a22d24
Michael Bestas <mkbestas@lineageos.org>
2018-05-24 21:10:56 +0300

    kuntao: Move NFC to vendor
    Change-Id: Ic23d56120d3302c95fb5101c9aef3a0fe8f8fcfb

--- device.mk
+++ device.mk
@@ -230,7 +230,7 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
+    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf
 
 # OMX
 PRODUCT_PACKAGES += \
