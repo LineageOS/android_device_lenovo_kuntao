a1e89e6e3e257b9d5e28c9608a7d0429f20d5225
Michael Bestas <mkbestas@lineageos.org>
2018-05-26 00:49:11 +0300

    kuntao: Add powerhint.xml
     * Based on LA.UM.6.6.r1-08500-89xx.0, adapted for the new format.
     * Required by perf_hint in libqti-perfd-client.so.
    
    Change-Id: Ief38dc242a92de05305452634fe2318e4de42f90

--- device.mk
+++ device.mk
@@ -282,6 +282,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
