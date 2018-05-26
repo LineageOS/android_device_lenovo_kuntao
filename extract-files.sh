324713a565d202a64948cec38468b3c4cd173265
Michael Bestas <mkbestas@lineageos.org>
2018-05-26 22:14:55 +0300

    kuntao: Patchelf blobs to remove deprecated dependencies
    * Remove android.hidl.base@1.0 & android.hidl.manager@1.0,
      they are included in libhidltransport since O-MR1
    
    Change-Id: I9338cfeb236b4b37e14c5d4e42af978ea8d54016

--- extract-files.sh
+++ extract-files.sh
@@ -61,8 +61,18 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 extract "$MY_DIR"/proprietary-files-twrp.txt "$SRC" "$SECTION"
 
-TWRP_QSEECOMD="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/recovery/root/sbin/qseecomd
+BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+TWRP_QSEECOMD="$BLOB_ROOT"/recovery/root/sbin/qseecomd
 
 sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_QSEECOMD"
+
+for HIDL_BASE_LIB in $(grep -lr "android\.hidl\.base@1\.0\.so" $BLOB_ROOT); do
+    patchelf --remove-needed android.hidl.base@1.0.so "$HIDL_BASE_LIB" || true
+done
+
+for HIDL_MANAGER_LIB in $(grep -lr "android\.hidl\.@1\.0\.so" $BLOB_ROOT); do
+    patchelf --remove-needed android.hidl.manager@1.0.so "$HIDL_MANAGER_LIB" || true
+done
 
 "$MY_DIR"/setup-makefiles.sh
