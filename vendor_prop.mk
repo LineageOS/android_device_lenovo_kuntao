7c15f9ede7cd28fc145f248ce77a37742f15c976
Michael Bestas <mkbestas@lineageos.org>
2018-05-26 15:26:10 +0300

    kuntao: Move device specific properties to vendor_prop.mk
    Change-Id: I8649131ad1e8000065944cac0a1f46623011c3e7

---
+++ vendor_prop.mk
@@ -0,0 +1,108 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    af.fast_track_multiplier=1 \
+    audio.deep_buffer.media=true \
+    audio.offload.min.duration.secs=30 \
+    audio.offload.video=true \
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    ro.vendor.audio.sdk.fluencetype=none \
+    vendor.audio_hal.period_size=192 \
+    vendor.audio.hw.aac.encoder=true \
+    vendor.audio.offload.buffer.size.kb=64 \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.offload.multiaac.enable=true \
+    vendor.audio.offload.multiple.enabled=false \
+    vendor.audio.offload.passthrough=false \
+    vendor.audio.offload.track.enable=true \
+    vendor.audio.parser.ip.buffer.size=262144 \
+    vendor.audio.playback.mch.downsample=true \
+    vendor.audio.safx.pbe.enabled=true \
+    vendor.voice.path.for.pcm.voip=true
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    qcom.bluetooth.soc=smd \
+    ro.qualcomm.bt.hci_transport=smd
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.camera.display.umax=1920x1080 \
+    persist.vendor.camera.display.lmax=1280x720 \
+    camera.lowpower.record.enable=1 \
+    vidc.enc.dcvs.extra-buff-count=2
+
+# CNE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.cne.feature=1
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.gralloc.enable_fb_ubwc=1 \
+    ro.opengles.version=196610 \
+    ro.qualcomm.cabl=0 \
+    ro.sf.lcd_density=480
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=1
+
+# Factory reset protection
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.frp.pst=/dev/block/platform/soc/7824900.sdhci/by-name/config
+
+# GPS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.gps.agps_provider=1
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+media.stagefright.audio.sink=280 \
+    vendor.vidc.dec.downscalar_width=1920 \
+    vendor.vidc.dec.downscalar_height=1088 \
+    vendor.vidc.disable.split.mode=1 \
+    vendor.vidc.enc.disable_bframes=1 \
+    vendor.vidc.enc.disable_pframes=1 \
+    vendor.vidc.enc.narrow.searchrange=1
+
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    persist.radio.apm_sim_not_pwdn=1 \
+    persist.radio.multisim.config=dsds \
+    ro.telephony.default_network=9,1 \
+    ro.telephony.call_ring.multiple=false \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.sib16_support=1 \
+    persist.data.netmgrd.qos.enable=true \
+    persist.data.mode=concurrent
+
+# Time services
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.delta_time.enable=true
+
+# WiFi
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0
