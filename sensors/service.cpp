b864c9cb4bf15e424ee918cc7ce8a287182dc4b9
Michael Bestas <mkbestas@lineageos.org>
2018-05-27 05:18:54 +0300

    kuntao: sensors: Convert to kuntao target
    Change-Id: Ic7de84be4f5706d7a1ca2f210819bf8606165974

--- sensors/service.cpp
+++ sensors/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.sensors@1.0-service"
+#define LOG_TAG "android.hardware.sensors@1.0-service.kuntao"
 
 #include <android/hardware/sensors/1.0/ISensors.h>
 #include <hidl/LegacySupport.h>
