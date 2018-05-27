c5a7c3d279dd2aa9730380fcbe0b97b3f31ea206
Michael Bestas <mkbestas@lineageos.org>
2018-05-27 05:18:54 +0300

    kuntao: sensors: Convert to kuntao target
    Change-Id: Ic7de84be4f5706d7a1ca2f210819bf8606165974

--- device.mk
+++ device.mk
@@ -330,7 +330,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service.kuntao
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
