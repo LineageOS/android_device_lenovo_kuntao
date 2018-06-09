fe98ae815814f9740a2f2c9808d99d62a0a5ee4b
Michael Bestas <mkbestas@lineageos.org>
2018-06-09 18:30:58 +0300

    kuntao: Build 1.0 power HAL
    * prima doesn't have support for wifi stats
    
    Change-Id: I351b88612907b52ab8f3c59d8040327988b8119c

--- device.mk
+++ device.mk
@@ -287,7 +287,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
