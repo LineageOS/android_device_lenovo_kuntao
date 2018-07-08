8969bac246c6168c8bdf50088e065223fba44446
Michael Bestas <mkbestas@lineageos.org>
2018-07-09 02:03:22 +0300

    kuntao: Add IMS symlinks
    Change-Id: I2228a8cc258a4eb06200edb7f1b70fd9e6e1d9a1

--- Android.mk
+++ Android.mk
@@ -29,6 +29,16 @@ include $(call all-subdir-makefiles)
 
 include $(CLEAR_VARS)
 
+IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "IMS lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
+
 RFS_MSM_ADSP_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/adsp/
 $(RFS_MSM_ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating RFS MSM ADSP folder structure: $@"
