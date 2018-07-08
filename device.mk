3ccc218e7d52ab84c37624d45a72a34d4976af59
Michael Bestas <mkbestas@lineageos.org>
2018-07-09 01:43:05 +0300

    kuntao: Move nfcee_access.xml to device tree
    Change-Id: I9d49d0116626987fa179c86ab7a63924f5d7fe1c

--- device.mk
+++ device.mk
@@ -230,7 +230,8 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf
+    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
 # OMX
 PRODUCT_PACKAGES += \
