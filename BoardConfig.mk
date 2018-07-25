ae68f69fe97e6f5e1ad4db4e395a5b0625fa3403
qq1484451453 <1484451453@qq.com>
2018-07-25 22:18:33 +0800

    kuntao: Add Asserts for P2c72(China version)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,7 +37,7 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
 # Asserts
-TARGET_OTA_ASSERT_DEVICE := kuntao,kuntao_row,P2a42,p2,p2a42
+TARGET_OTA_ASSERT_DEVICE := kuntao,kuntao_row,P2a42,p2,p2a42,p2c72,P2c72
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-uart"
