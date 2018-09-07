d35add27c34288de2c7439fea4877b64188d4641
LuK1337 <priv.luk@gmail.com>
2018-09-07 15:21:20 +0200

    kuntao: Drop libbt-vendor
    * With QTI bluetooth it's no longer needed.
    
    Change-Id: Iba4b0b6ba24fe1b85e7175291ed84769615ffadd

--- device.mk
+++ device.mk
@@ -70,10 +70,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
-# Bluetooth
-PRODUCT_PACKAGES += \
-    libbt-vendor
-
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8953 \
