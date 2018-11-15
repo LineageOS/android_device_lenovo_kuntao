f7eab807b555adef9e0191577cdf77501a92f02b
Yifan Hong <elsk@google.com>
2018-11-14 22:58:44 -0700

    kuntao: Health: Uprevision to 2.0
    Test: adb logcat -e health
    Test: lshal --debug
    Change-Id: I3855f256d79a46c9626f043aac667937ff6414e1

--- device.mk
+++ device.mk
@@ -155,8 +155,8 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # IMS
 PRODUCT_PACKAGES += \
