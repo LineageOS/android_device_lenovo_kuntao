629d79ba19cf8f3198798604550029002e2e59ed
Bruno Martins <bgcngm@gmail.com>
2018-12-22 19:07:24 +0000

    kuntao: Enable B-services aging propagation
    Change-Id: I4ac0525933bb421f7d025c5e4c6a1de0f083c46e

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -96,6 +96,10 @@ media.stagefright.audio.sink=280 \
     vendor.vidc.enc.disable_pframes=1 \
     vendor.vidc.enc.narrow.searchrange=1
 
+# Memory optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.sys.fw.bservice_enable=true
+
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so
