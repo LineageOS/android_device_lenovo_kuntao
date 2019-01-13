40f2e5946e5ab18cfa6fac193cc3f98279864f2c
Michael Bestas <mkbestas@lineageos.org>
2019-01-13 07:12:14 +0200

    kuntao: Convert lineagehw to vendor.lineage.touch
    TODO: Implement FP gestures in a different way
    
    Change-Id: I1174a4dc608dadd12b9eff8a73eed7c3a6917ec6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,9 +127,6 @@ TARGET_KERNEL_SOURCE := kernel/lenovo/msm8953
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
-# Lineage Hardware
-JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
-
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
