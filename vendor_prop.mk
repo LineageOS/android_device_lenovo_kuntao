e62b3cfe166a522e9bd947983dbe38f57e312bbf
Michael Bestas <mkbestas@lineageos.org>
2019-04-13 18:22:23 +0300

    kuntao: Add vendor prefix to yet another prop
    * Fix network breakage after toggling airplane mode
    
    Change-Id: I91fc2a3f879b075970a75a2a5133f385f855186e

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -108,10 +108,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    persist.radio.apm_sim_not_pwdn=1 \
     persist.radio.multisim.config=dsds \
     ro.telephony.default_network=9,1 \
     ro.telephony.call_ring.multiple=false \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1 \
