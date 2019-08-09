9118f037bb469dbdd0c444ec92404aa9d4eeec30
Han Wang <416810799@qq.com>
2019-08-09 14:15:07 +0200

    kuntao: Override vendor.sensors-hal-1-0 instead of building custom service
     * We can override it in init scripts, no need to create a separate service.
    
    Change-Id: I716fb703373f0b9e8b885707fa72a76957cfdcf5

--- device.mk
+++ device.mk
@@ -339,7 +339,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service.kuntao
+    android.hardware.sensors@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
