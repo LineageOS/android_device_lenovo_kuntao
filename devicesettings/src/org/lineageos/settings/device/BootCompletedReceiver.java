451688d2a5fc0271c854df4092ee031f0a799403
Bruno Martins <bgcngm@gmail.com>
2019-09-01 13:05:37 +0100

    kuntao: devicesettings: Migrate to androidx
     * Migrate to SDK version 29
    
    Change-Id: I8d92ee6b6ec5fe42a543a8ad02867eb48e2dcc04

--- devicesettings/src/org/lineageos/settings/device/BootCompletedReceiver.java
+++ devicesettings/src/org/lineageos/settings/device/BootCompletedReceiver.java
@@ -20,7 +20,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+
+import androidx.preference.PreferenceManager;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
