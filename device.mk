b68447bdd534b66a797ed66841da012d4171c2b4
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-09 16:52:33 +0200

    kuntao: Move soong namespaces to device.mk
    * This is a product property
    
    Change-Id: I411428687839fca7c794d488bb48e5f1773e570b

--- device.mk
+++ device.mk
@@ -348,6 +348,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
