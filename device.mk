32c4cfb0df66a94b4fa6370e97e15acd4c081421
Ethan Chen <intervigil@gmail.com>
2020-10-11 15:07:35 -0700

    kuntao: Move telephony packages to /product
    Change-Id: I893eff0ca79bb6eb4ac53030eb6d1a3e1ad54b5b

--- device.mk
+++ device.mk
@@ -303,7 +303,7 @@ PRODUCT_COPY_FILES += \
 
 # QCOM
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/product-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # RCS
