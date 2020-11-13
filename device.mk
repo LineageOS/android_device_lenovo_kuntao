2168458195c463ee74ac4f5fcc08677b479482d2
Bruno Martins <bgcngm@gmail.com>
2020-11-13 10:56:44 +0000

    kuntao: Explicitly build additional IMS stack dependency
    Commit 295a82a removed the lib from PRODUCT_PACKAGES with the
    assumption that it was implicitely built while having
    TARGET_USES_QTI_CAMERA flag set. Turns out that build system
    skips compilation of vendor.qti.hardware.camera.device@1.0
    that is supposed to land in system_ext/lib64 and this one is
    a required dependency of the current IMS stack.
    
    Change-Id: I7fdbdc1b837df670ecaf6775946784017c68134c

--- device.mk
+++ device.mk
@@ -85,7 +85,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4:64 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5:64
+    android.hardware.camera.provider@2.5:64 \
+    vendor.qti.hardware.camera.device@1.0:64
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
