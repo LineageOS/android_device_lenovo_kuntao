78f530b8fb0d62f920f0264b1606948ff399f226
Michael Bestas <mkbestas@lineageos.org>
2020-12-29 22:35:50 +0200

    DNM permissive selinux
    Change-Id: I81e01e8b14d3e044ec9af63f4a46fdca003c750a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,7 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_CONFIG := lineageos_kuntao_defconfig
