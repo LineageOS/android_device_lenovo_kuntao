e60944bb4b6d78b014e235bfed010860d851abd4
Michael Bestas <mkbestas@lineageos.org>
2021-01-09 02:57:17 +0200

    kuntao: Copy libdexfile_support in VNDK apex
    otherwise it fails to link and RS is loop crashing
    
    Change-Id: I8046b89d412c91c2674126235c0167a0c0388d39

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -114,4 +114,49 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := \
     $(addsuffix .vndk-ext-gen,$(EXTRA_VENDOR_LIBRARIES))
 include $(BUILD_PHONY_PACKAGE)
+
+EXTRA_VNDK_APEX_LIBRARIES := \
+    libdexfile_support
+
+define define-vndk-apex-lib
+include $$(CLEAR_VARS)
+LOCAL_MODULE := $1.$2
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_PREBUILT_MODULE_FILE := $$(call intermediates-dir-for,SHARED_LIBRARIES,$1)/$1.so
+LOCAL_STRIP_MODULE := false
+LOCAL_MULTILIB := first
+LOCAL_MODULE_TAGS := optional
+LOCAL_INSTALLED_MODULE_STEM := $1.so
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/apex/com.android.vndk.current/lib64
+LOCAL_CHECK_ELF_FILES := false
+include $$(BUILD_PREBUILT)
+
+ifneq ($$(TARGET_2ND_ARCH),)
+ifneq ($$(TARGET_TRANSLATE_2ND_ARCH),true)
+include $$(CLEAR_VARS)
+LOCAL_MODULE := $1.$2
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_PREBUILT_MODULE_FILE := $$(call intermediates-dir-for,SHARED_LIBRARIES,$1,,,$(TARGET_2ND_ARCH_VAR_PREFIX))/$1.so
+LOCAL_STRIP_MODULE := false
+LOCAL_MULTILIB := 32
+LOCAL_MODULE_TAGS := optional
+LOCAL_INSTALLED_MODULE_STEM := $1.so
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/apex/com.android.vndk.current/lib
+LOCAL_CHECK_ELF_FILES := false
+include $$(BUILD_PREBUILT)
+endif # TARGET_TRANSLATE_2ND_ARCH is not true
+endif # TARGET_2ND_ARCH is not empty
+endef
+
+$(foreach lib,$(EXTRA_VNDK_APEX_LIBRARIES),\
+    $(eval $(call define-vndk-apex-lib,$(lib),vndk-apex-ext-gen)))
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := vndk-apex-ext
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := \
+    $(addsuffix .vndk-apex-ext-gen,$(EXTRA_VNDK_APEX_LIBRARIES))
+include $(BUILD_PHONY_PACKAGE)
 endif
